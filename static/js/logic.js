7e599583662d1b78ecdb77af7b91db7c948fa43a
ghhyc <yenth602@gmail.com>
2022-01-31 14:11:41 -0800

    move files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,41 +1,42 @@
-// first step: create the map! day 1 activity 1
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
-//----------------------------------------------------------------------------
-// var myMap = L.map("map", {
-//   center: [37.7749, -122.4194],
-//   zoom: 5
-//   });
+var myMap = L.map("map", {
+  center: [45.52, -122.67],
+  zoom: 13,
+});
 
 // Adding a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
-// 
+//
+//------------
+0;
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken:
+      "pk.eyJ1IjoieXRoNjAyIiwiYSI6ImNreXRwMjhtNzFmcXMydm85cWoya3dlMjUifQ.y2-wA_382EIfDx3L3vwSsA",
+  }
+).addTo(myMap);
 
-  // L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  //   maxZoom: 18,
-  //   id: "mapbox.streets",
-  //   accessToken: "pk.eyJ1IjoieXRoNjAyIiwiYSI6ImNreXBjdm4xcTA4czYydW55ejhzdjN2Z3EifQ.Iirk2dVnTy_PNUlpeUlsNg"
-  // }).addTo(myMap);
-//------------------------------------------------------------->
-//pk.eyJ1IjoieXRoNjAyIiwiYSI6ImNreXBjdm4xcTA4czYydW55ejhzdjN2Z3EifQ.Iirk2dVnTy_PNUlpeUlsNg
 // Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var queryUrl =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 // Perform a GET request to the query URL/
 
 d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   // createFeatures(data.features);
-
   // example to print out the data before continuing
-  console.log(data)
-
-  // second step: create the markers, popups, legend
-
-  // 2.1 Markers: magnitude = size, depth = color. inc mag = bigger circle, inc depth = darker color
-  // depth is found in coordinate next to lat and long
-
+  //
+  //console.log(data)
+  //
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -44,16 +45,12 @@ d3.json(queryUrl).then(function (data) {
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
-      weight: 0.5
-    };  
-  }  
-  // 2.2: Popups: add information about earthquake (add title and time to popup) 
-
-  // 2.3: Legend: add legend. information about the depth of the earthquake. 
-  //      higher depth = darker color, lower depth = lighter color
-  // set different color from magnitude
-    function chooseColor(magnitude) {
-      switch (true) {
+      weight: 0.5,
+    };
+  }
+  // set color from magnitude
+  function getColor(magnitude) {
+    switch (true) {
       case magnitude > 5:
         return "#ea2c2c";
       case magnitude > 4:
@@ -66,62 +63,64 @@ d3.json(queryUrl).then(function (data) {
         return "#d4ee00";
       default:
         return "#98ee00";
-      }
     }
-    // set radiuss from magnitude
-    function getRadius(magnitude) {
-      if (magnitude === 0) {
-        return 1;
-    }
-    return magnitude * 4; 
   }
-
-    // GeoJSON layer
-    L.geoJson(data, {
-      // Maken cricles
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-      // cirecle style
-      style: styleInfo,
-      // popup for each marker
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup(
-          "Magnitude: " 
-          + feature.properties.mag 
-          + "<br>Location: " 
-          + feature.properties.place);  
-  }  
-}).addTo(myMap);
-
-  // add legend
+  // set radius from magnitue
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+  // GeoJSON layer
+  L.geoJson(data, {
+    // Maken cricles
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // cirecle style
+    style: styleInfo,
+    // popup for each marker
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: " +
+          feature.properties.mag +
+          "<br>Location: " +
+          feature.properties.place
+      );
+    },
+  }).addTo(myMap);
+  // an object legend
   var legend = L.control({
-    position: "bottomright"
+    position: "bottomright",
   });
 
-  // Add all details for the legend
-  legend.onAdd = function() {
+  // details for the legend
+  legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
 
-    // legend description
-    var grades = [-10, 10, 30, 50, 70, 90];
+    var grades = [0, 1, 2, 3, 4, 5];
     var colors = [
       "#98ee00",
       "#d4ee00",
       "#eecc00",
       "#ee9c00",
-      "#e95d26",
-      "#e92684"
+      "#ea822c",
+      "#ea2c2c",
     ];
 
-
-    // loop all intervals to generate color lable for each interval  
+    // Looping through
     for (var i = 0; i < grades.length; i++) {
-      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
-      + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-    } 
+      div.innerHTML +=
+        "<i style='background: " +
+        colors[i] +
+        "'></i> " +
+        grades[i] +
+        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
     return div;
   };
-    // Add legend to the map.
-    legend.addTo(myMap);
+
+  // Finally, we our legend to the map.
+  legend.addTo(myMap);
 });
