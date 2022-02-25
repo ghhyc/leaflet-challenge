b0b97bea3a4cce698e41098247d764bd98ef4fc3
ghhyc <yenth602@gmail.com>
2022-02-25 15:04:23 -0800

    update PORT for Heroku in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// Etheir be the enviroment variable PORT or will be 3000 if there is not enviroment
+const port = process.env.PORT || 3000;
 app.use(express.static("static"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
